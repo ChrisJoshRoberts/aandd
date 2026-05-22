844082c4ae99043f56d117ac409f2428342153d5
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2026-05-22 16:04:44 +0200

    test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,6 +14,7 @@ export default async function Home() {
           <p>{testimonial.testimonial}</p>
         </div>
       ))}
+      {/* add */}
       <Link href="/web-design">Web Design Page</Link>
     </div>
   );
